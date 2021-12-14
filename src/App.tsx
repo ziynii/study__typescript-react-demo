9367f90d3ee5b1d021b248725e629826d827c266
Jiyeon <yeeoni_@naver.com>
2021-12-14 15:43:10 +0900

    [chapter3] react event + typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,37 @@ import './App.css';
 import styled from 'styled-components';
 import React, { Component } from 'react';
 import Number from './Number';
+import { Form, Input } from './Input';
 
 interface IState {
   counter: number;
+  name: string;
 }
 
 class App extends Component<{}, IState> {
   state = {
     counter: 0,
+    name: '',
   };
   render() {
-    const { counter } = this.state;
+    const { counter, name } = this.state;
     return (
       <div>
+        <Form onFormSubmit={this.onFormSubmit}>
+          <Input value={name} onChange={this.onChange} />
+        </Form>
         <Number count={counter} /> <button onClick={this.add}>Add</button>
       </div>
     );
   }
+
+  onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  };
+
+  onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
 
   add = () => {
     this.setState((prev) => {
